45cefc545648633b045f4919de4a065cb7760e9b
Pierre Gunter <halfdood@gmail.com>
2017-01-06 10:29:27 +0200

    Add the default route. Add the "system select" route.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { NotFoundComponent } from './not-found/not-found.component';
+import { SystemSelectComponent } from './system-select/system-select.component';
 
 const appRoutes: Routes = [
+  { path: 'system-select', component: SystemSelectComponent },
+  { path: '', redirectTo: '/system-select', pathMatch: 'full' },
   { path: '**', component: NotFoundComponent }
 ];
 
