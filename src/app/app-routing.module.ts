d415662f55124c60ec11a241ca97e1584db25dcf
Pierre Gunter <halfdood@gmail.com>
2017-01-06 10:18:39 +0200

    Add the 404 route for unknown pages.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const appRoutes: Routes = [];
+import { NotFoundComponent } from './not-found/not-found.component';
+
+const appRoutes: Routes = [
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   exports: [
