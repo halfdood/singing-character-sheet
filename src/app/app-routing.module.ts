f81390c64556be86fd8d8df979789c1dd6842e3b
Pierre Gunter <halfdood@gmail.com>
2017-01-06 11:19:52 +0200

    Register the FAE route.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { FaeComponent } from './fae/fae.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { SystemSelectComponent } from './system-select/system-select.component';
 
 const appRoutes: Routes = [
+  { path: 'fae', component: FaeComponent },
   { path: 'system-select', component: SystemSelectComponent },
   { path: '', redirectTo: '/system-select', pathMatch: 'full' },
   { path: '**', component: NotFoundComponent }
