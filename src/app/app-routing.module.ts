a6c8da824723f527fe0ad63296f88d5f4213bfa4
Pierre Gunter <pierre.gunter@supergrp.net>
2017-01-18 15:48:33 +0200

    Add the character select routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { FaeComponent } from './fae/fae.component';
+import { CharacterSelectComponent } from './fae/character-select/character-select.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { SystemSelectComponent } from './system-select/system-select.component';
 
 const appRoutes: Routes = [
-  { path: 'fae', component: FaeComponent },
+  { path: 'fae',
+    children: [
+      { path: 'characters', component: CharacterSelectComponent },
+      { path: ':name', component: FaeComponent },
+      { path: '', redirectTo: '/fae/characters', pathMatch: 'full' }
+    ]
+  },
   { path: 'system-select', component: SystemSelectComponent },
   { path: '', redirectTo: '/system-select', pathMatch: 'full' },
   { path: '**', component: NotFoundComponent }
