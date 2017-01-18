6638eb21cf8aa19ac925ae11060ff3336451ca6b
Pierre Gunter <pierre.gunter@supergrp.net>
2017-01-18 16:56:13 +0200

    Add implementation for the characterservice.all.
    Add the get set functions for the store service.

--- src/app/fae/services/characters.service.ts
+++ src/app/fae/services/characters.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 
 import { StoreService } from '../../services/store.service';
 
+const ALL = 'fae_characters_all';
+
 @Injectable()
 export class CharactersService {
 
@@ -9,6 +11,6 @@ export class CharactersService {
     private store: StoreService) { }
 
   all (): string[] {
-    return [];
+    return JSON.parse(this.store.get(ALL)) as string[] || [];
   }
 }
--- src/app/services/store.service.ts
+++ src/app/services/store.service.ts
@@ -5,4 +5,11 @@ export class StoreService {
 
   constructor() { }
 
+  get (key: string): string {
+    return localStorage.getItem(key);
+  }
+
+  set (key: string, data: string) {
+    localStorage.setItem(key, data);
+  }
 }
